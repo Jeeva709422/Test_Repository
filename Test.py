572d0d9fee7a2125639820a308c2da705d50c2de
Jeevas <jeevas@cdmsmith.com>
2025-08-28 10:39:19 +0530

    Your commit message

--- Test.py
+++ Test.py
@@ -1,2 +1,11 @@
-#This is for testing purpose
-print("Test")
+# This is for testing purpose
+i = 3  # initialize i with some value
+
+if i < 5:
+    print("i is less than 5")
+    i += 1
+    if i == 5:
+        print("i is equal to 5")
+else:
+    print("i is greater than 5")
+print("Final value of i:", i)
